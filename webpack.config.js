180bbd4f9426ba97e8a90c666959df7f91df32d4
Hadar Alon <hadaralon@h-MacBook-Pro-sl-Hadar.local>
2022-03-15 00:22:42 +0200

    bundle

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const glob = require("glob");
+
+module.exports = {
+    mode: 'development',
+    entry: glob.sync('./src/js/*.js').reduce(function (obj, el) {
+        if (obj['main']) obj['main'].push(el)
+        else obj['main'] = []
+        return obj
+    }, {}),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true,
+    },
+    resolve: {
+        preferRelative: true,
+    },
+};
